dae6d9358e1e20963034991f124802514f08eba1
decoded-cipher <arjun12345krishna@gmail.com>
2020-01-11 03:20:46 +0530

    Passenger Activity page design completed:

--- app/src/main/java/com/example/entebus/MainActivity.java
+++ app/src/main/java/com/example/entebus/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.entebus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next;
+    Intent nxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        next = findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                nxt = new Intent(getApplicationContext(), PassengerActivity.class);
+                startActivity(nxt);
+
+            }
+        });
     }
 }
