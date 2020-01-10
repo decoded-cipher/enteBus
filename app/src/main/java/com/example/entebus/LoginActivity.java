6899ee1d487806cfaa246fc327a26952ab703f9f
decoded-cipher <arjun12345krishna@gmail.com>
2020-01-10 11:08:44 +0530

    Login and User registration page connected - INTEND

--- app/src/main/java/com/example/entebus/LoginActivity.java
+++ app/src/main/java/com/example/entebus/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.entebus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText username, password;
+    Button login, register;
+    String usrname, passwrd, user, pass;
+    Intent log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                log = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(log);
+            }
+        });
+
     }
 }
