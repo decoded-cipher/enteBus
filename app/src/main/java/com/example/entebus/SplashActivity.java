cebd0afb5e0d01e4f252e6d755490a9a0e54c0d9
decoded-cipher <arjun12345krishna@gmail.com>
2020-01-13 00:11:04 +0530

    Splash screen with Animation, completed

--- app/src/main/java/com/example/entebus/SplashActivity.java
+++ app/src/main/java/com/example/entebus/SplashActivity.java
@@ -2,13 +2,41 @@ package com.example.entebus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    ImageView splash_image;
+    Intent start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        start = new Intent(getApplicationContext(), LoginActivity.class);
+
+        splash_image = findViewById(R.id.splash);
+        Animation splash = AnimationUtils.loadAnimation(this,R.anim.splash_transition);
+        splash_image.startAnimation(splash);
+
+        Thread splash_timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(start);
+                    finish();
+                }
+            }
+        };
+        splash_timer.start();
     }
 }
